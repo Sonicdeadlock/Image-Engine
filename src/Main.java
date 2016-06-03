18d8930be626b19bed38039d83eb6acd278267ca
sonicdeadlock <sonicdeadlock369@gmail.com>
2016-06-02 20:55:40 -0400

    removed files from old structure

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
+import render.CharacterSet;
+import render.ImageToAsciiImage;
+import render.ImageToAsciiText;
+import render.Settings;
+
 import java.io.File;
 
 public class Main {
 
     public static void main(String[] args) {
-	    Settings s = new Settings(.21,new File("J:\\Norfolk_Southern_,_Kingwood_Texas.jpg"));
-        s.setCharacterSet(new CharacterSet("norfolksouthermNORFOLKSOUTHERN ",s.getTolerance()));
-        ImageToAsciiImage tiai = new ImageToAsciiImage(s);
-        tiai.renderImages();
+//	    Settings s = new Settings(.1,new File("C:\\Users\\VYTKB\\Pictures\\23626.jpg"));
+	    Settings s = new Settings(.1,new File("H:\\wallpapers"));
+        s.setCharacterSet(new render.CharacterSet("norfolksouthermNORFOLKSOUTHERN ",s.getTolerance()));
+//        s.setCharacterSet(new CharacterSet("chair ",s.getTolerance()));
+//        ImageToAsciiImage tiai = new ImageToAsciiImage(s);
+//        tiai.renderImages();
+        ImageToAsciiText itat = new ImageToAsciiText(s);
+        itat.renderText();
     }
 }
